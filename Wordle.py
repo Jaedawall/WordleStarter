cb0f0182ed51bea848663588863b326d3c6ca12c
Jaewall <jaedawaldram@gmail.com>
2023-09-13 16:35:47 -0600

    Added verification and show the word

--- Wordle.py
+++ Wordle.py
@@ -30,18 +30,21 @@ def wordle():
         # Initialize lists to keep track of correctly guessed letters and their positions
         correct_letters = []
         correct_positions = []
-        
+        guessed_words = []
+        correct_position = 0
+        correct_letter = 0
         # Check each letter in the typed word against the random word
         
         for i in range(N_COLS):
             typed_letter = typed_word[i]
             random_letter = Solution[i]
-            
             if typed_letter == random_letter:
+                correct_position += 1
                 # Letter is correct and in the correct position
                 correct_letters.append(typed_letter)
                 correct_positions.append(i)
             elif typed_letter in Solution and typed_word.count(typed_letter) <= Solution.count(typed_letter):
+                correct_letter += 1
                 # Letter is correct but in the wrong position
                 correct_letters.append(typed_letter)
         
@@ -60,13 +63,23 @@ def wordle():
         else:
             # Check if the typed word is a legitimate English word
             if typed_word.lower() in FIVE_LETTER_WORDS:
+                gw.show_message(str(correct_position) + " correct position " + '\n' + str(correct_letter) + " in word.")
                 # Move to the next row
+                guessed_words.append(typed_word)
                 current_row = gw.get_current_row()
+                
                 if current_row < N_ROWS - 1:
                     gw.set_current_row(current_row + 1)
+            elif len(typed_word) != 4:
+                gw.show_message("Not enough letters")
+                gw.set_current_row(current_row)
             else:
                 gw.show_message("Not in word list")
                 gw.set_current_row(current_row)
+            
+        if current_row == N_ROWS - 1:
+            gw.show_message("Game over! The word was " + Solution)
+            gw.exit_on_close()
 
 
     gw.add_enter_listener(enter_action)
